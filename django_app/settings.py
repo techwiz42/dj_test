109a093d4c6708e2d6e69db8a4367885f65c6d07
Peter Sisk <thetechwizard42@gmail.com>
2023-08-20 23:02:18 +0000

    zeroth version of working django project

--- django_app/settings.py
+++ django_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hello_world'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'django_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/projects/dj_test/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
